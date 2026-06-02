a6f834668e298c744523652dfff737f189fec7a4
SkyLuckee <locnd07@gmail.com>
2026-06-02 11:05:33 +0700

    feat: UI elements variables

--- Assets/Scripts/Managers/DataLoader.cs
+++ Assets/Scripts/Managers/DataLoader.cs
@@ -7,6 +7,9 @@ using Cysharp.Threading.Tasks;
 using MajSimai;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
+using System.Text.RegularExpressions;
+using TMPro;
 
 #endregion
 
@@ -48,6 +51,23 @@ public class DataLoader : MonoBehaviour
     public Text designText;
     public RawImage cardImage;
     public Color[] diffColors = new Color[7];
+    public TextMeshProUGUI levelTextM;
+    public Text titleTextM;
+    public Text artistTextM;
+    public Text designTextM;
+    // public Text bpmTextM;
+    public SpriteRenderer cardImageM;
+    public SpriteRenderer LvBackgroundM;
+    // public SpriteRenderer[] TabM = new SpriteRenderer[2];
+    // public GameObject[] Modes = new GameObject[2];
+    public Sprite[] cardImagesM = new Sprite[8];
+    public Sprite[] LvBackgroundsM = new Sprite[8];
+    // public Sprite[] TabsM = new Sprite[8];
+    public Texture2D[] MLevelsM = new Texture2D[8];
+    public GameObject QuestionM;
+    // public GameObject TabUTGM;
+    // public Text UTGTextM;
+    // public GameObject TabUTG2pM;
 
     private const double StreamingCreatePreloadTime = 4;
     private const double StreamingFrameBudgetMs = 4;
@@ -224,6 +244,49 @@ public class DataLoader : MonoBehaviour
         levelText.text = chart.Level;
         designText.text = chart.Designer;
         this.legacySlideLayer = legacySlideLayer;
+
+        //MaiUI
+        levelTextM.spriteAsset.spriteSheet = MLevelsM[diff];
+        levelTextM.spriteAsset.material.SetTexture("_MainTex", MLevelsM[diff]);
+
+        StringBuilder sb = new();
+        if (chart.Level.Length == 1)
+        {
+            sb.Append("<space=1>");
+        }
+        foreach (var item in chart.Level)
+        {
+            if (int.TryParse(item.ToString(), out int lv))
+                sb.Append($"<sprite={lv}>");
+            else
+            {
+                switch (item)
+                {
+                    case '+':
+                        sb.Append("<sprite=10>");
+                        break;
+                    case '-':
+                        sb.Append("<sprite=11>");
+                        break;
+                    case ',':
+                        sb.Append("<sprite=12>");
+                        break;
+                    case '.':
+                        sb.Append("<sprite=13>");
+                        break;
+                }
+            }
+        }
+        levelTextM.text = sb.ToString();
+        titleTextM.text = title;
+        artistTextM.text = artist;
+        designTextM.text = chart.Designer;
+        // bpmTextM.text = "BPM " + loadedData.WholeBpm;
+        cardImageM.sprite = cardImagesM[diff];
+        LvBackgroundM.sprite = LvBackgroundsM[diff];
+
+        QuestionM.SetActive(chart.Level.EndsWith('?'));
+        chart.Level = chart.Level.Replace("?", "");
 
         objectCounter.CountNoteSumAsync(chart).Forget();
         objectCounter.ReportMeterBpmAsync(chart).Forget();
